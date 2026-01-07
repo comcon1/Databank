3b0b24e3bc2cd7d817a8feca6b2c67e96f828fb2
Alexey Nesterenko <comcon1@protonmail.com>
2026-01-07 14:39:53 +0100

    fix license key

--- src/fairmd/lipids/__init__.py
+++ src/fairmd/lipids/__init__.py
@@ -14,11 +14,11 @@ import sys
 from ._version import __version__
 
 # Package Information
-_mtd = importlib.metadata.metadata("fairmd-lipids")
-__author_email__ = _mtd["Author-email"]
+_mtd = importlib.metadata.metadata("fairmd.lipids")
+__author_email__ = _mtd.get("Author-email", "unknown")
 __author__ = re.sub(r" ?<.*>$", "", __author_email__)
-__description__ = _mtd["Summary"]  # reads toml's `description` field
-__license__ = _mtd["Licence-Expression"]
+__description__ = _mtd.get("Summary")  # reads toml's `description` field
+__license__ = _mtd.get("License-Expression", "no licence")
 __url__ = _mtd.json["project_url"][2].split(", ")[1]  # url nr 3
 
 # Global Paths
