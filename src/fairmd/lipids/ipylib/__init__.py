155da6cd820bd4085d35400b3c5626ef518decd9
Alexey Nesterenko <comcon1@protonmail.com>
2026-01-14 08:50:24 +0100

    Remove showTable as it is not in use anymore. Qualities are already in tables.

--- src/fairmd/lipids/ipylib/__init__.py
+++ src/fairmd/lipids/ipylib/__init__.py
@@ -1,6 +1,5 @@
 """Routines for IPython/Jupyter notebooks."""
 
-from .jpyroutines import showTable
 from .plottings import plotFormFactor, plotOrderParameters, plotSimulation
 
 __all__ = ["plotFormFactor", "plotOrderParameters", "plotSimulation", "showTable"]
