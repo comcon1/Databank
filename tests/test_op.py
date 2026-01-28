aeec4d8c4475e1e9a45e5b6c4db4a635a9eefb22
Alexey Nesterenko <comcon1@protonmail.com>
2026-01-28 11:31:56 +0100

    Implement OPdict via NamingRegistry

--- tests/test_op.py
+++ tests/test_op.py
@@ -30,7 +30,16 @@ class TestBuildNiceOPdict:
 
         return initialize_databank()
 
-    def test_build_nice_OPdict(self, systems):
+    def test_work_empty(self):
+        from fairmd.lipids.auxiliary.opconvertor import build_nice_OPdict
+        from fairmd.lipids.molecules import Lipid
+
+        lipid = Lipid("POPE")
+        lipid.register_mapping()
+        rdict = build_nice_OPdict({}, lipid)
+        assert rdict == {}
+
+    def test_build_fragmented(self, systems):
         from fairmd.lipids.auxiliary.opconvertor import build_nice_OPdict
         from fairmd.lipids.api import get_OP
 
@@ -40,3 +49,27 @@ class TestBuildNiceOPdict:
         assert isinstance(rdict, dict)  # dict expected
         assert "sn-1" in rdict
         assert "sn-2" in rdict  # fragments at the top level
+
+    def test_cnames_pl(self, systems):
+        from fairmd.lipids.auxiliary.opconvertor import build_nice_OPdict
+        from fairmd.lipids.api import get_OP
+
+        sys = systems.loc(281)
+        opdata = get_OP(sys)
+        rdict = build_nice_OPdict(opdata["POPC"], sys.lipids["POPC"])
+
+        # C check numbers
+        def has_c(cname: str, flist: dict) -> bool:
+            return any(_c["C"] == cname for _c in flist)
+
+        check.is_true(has_c("2", rdict["sn-1"]))
+        check.is_true(has_c("16", rdict["sn-1"]))
+        check.is_false(has_c("1", rdict["sn-1"]))
+        check.is_false(has_c("17", rdict["sn-1"]))
+        # H check names
+        check.is_true(all(_c["H"] in ["1", "2", "3"] for _c in rdict["sn-1"]))
+        check.is_true(all(_c["H"] in ["1", "2", "3"] for _c in rdict["sn-2"]))
+        # check backbone
+        check.is_true(has_c("g1", rdict["glycerol backbone"]))
+        check.is_true(has_c("g2", rdict["glycerol backbone"]))
+        check.is_true(has_c("g3", rdict["glycerol backbone"]))
