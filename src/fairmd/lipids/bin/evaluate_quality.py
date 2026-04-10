a823aa6feb3f8ba29ad32986f62b0f15bac62232
Alexey Nesterenko <comcon1@protonmail.com>
2026-04-11 01:11:34 +0200

    develop FF quality into three-factor quality

--- src/fairmd/lipids/bin/evaluate_quality.py
+++ src/fairmd/lipids/bin/evaluate_quality.py
@@ -9,9 +9,9 @@ In the standard protocol, it should be run *after* :ref:`fmdl_match_experiments 
 
 .. code-block:: console
 
-    fmdl_evaluate_quality [--op-only | --ff-only]
+    fmdl_evaluate_quality [--op-only | --ff-only] [ --idlist=414,536 ]
 
-No arguments means that both OP and FF qualities are evaluated.
+No arguments means that both OP and FF qualities are evaluated for everything.
 """
 
 import argparse
@@ -35,6 +35,7 @@ def _evaluate_ff_qualities(simulations) -> int:
     counter = 0
     ffexps = ExperimentCollection.load_from_data("FFExperiment")
     for simulation in simulations:
+        print("Evaluating Sim#%d:" % simulation["ID"])
         evaluator = qq.FFQualityEvaluator(simulation, ffexps)
         if evaluator.evaluate_one():
             evaluator.save_results()
@@ -42,8 +43,8 @@ def _evaluate_ff_qualities(simulations) -> int:
     return counter
 
 
-def evaluate_quality_impl(*, do_op: bool = True, do_ff: bool = True) -> None:
-    simulations = qq.QualSimulation.load_all_paired()
+def evaluate_quality_impl(id_list: list[int] | None = None, *, do_op: bool = True, do_ff: bool = True) -> None:
+    simulations = qq.QualSimulation.load_all_paired(id_list)
 
     evaluated_op_counter = _evaluate_op_qualities(simulations) if do_op else 0
     evaluated_ff_counter = _evaluate_ff_qualities(simulations) if do_ff else 0
@@ -64,12 +65,18 @@ def evaluate_quality():
         action="store_true",
         help="Evaluate form factors only (skip order parameters)",
     )
+    parser.add_argument(
+        "--idlist",
+        type=lambda s: [int(x) for x in s.split(",")],
+        default=None,
+        help="Comma-separated list of IDs to evaluate (e.g. 1,2,3)",
+    )
     args = parser.parse_args()
     if args.op_only and args.ff_only:
         msg = "Incompatible arguments. Please choose one."
         raise RuntimeError(msg)
-
     evaluate_quality_impl(
+        id_list=args.idlist,
         do_op=not args.ff_only,
         do_ff=not args.op_only,
     )
